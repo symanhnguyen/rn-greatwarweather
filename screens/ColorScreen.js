45809784da6b378d448fa5b99391f4b633c51d5a
symanhnguyen <63956316+symanhnguyen@users.noreply.github.com>
2022-09-17 16:41:45 +0700

    firstbattle

---
+++ screens/ColorScreen.js
@@ -0,0 +1,48 @@
+import {
+  StyleSheet,
+  Appearance,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
+import React from "react";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { StatusBar } from "expo-status-bar";
+
+export default function ColorScreen() {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle =
+    colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === "light" ? styles.lightContainer : styles.darkContainer;
+
+  return (
+    <View style={[styles.container, themeContainerStyle]}>
+      <Text style={[styles.text, themeTextStyle]}>
+        Color scheme: {colorScheme}
+      </Text>
+      <StatusBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  lightContainer: {
+    backgroundColor: "#d0d0c0",
+  },
+  darkContainer: {
+    backgroundColor: "#242c40",
+  },
+  lightThemeText: {
+    color: "#242c40",
+  },
+  darkThemeText: {
+    color: "#d0d0c0",
+  },
+});
---
+++ screens/LearnScreen.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import React from "react";
+
+const LearnScreen = () => {
+  const isDarkMode = useColorScheme() === "dark";
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}
+      >
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}
+        >
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default LearnScreen;
+
+const styles = StyleSheet.create({});
